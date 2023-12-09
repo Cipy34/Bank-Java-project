b3a19d8ea082598f35a895b1756d992998a49b31
Cipy348 <cipriangabriel112@gmail.com>
2023-12-09 20:49:17 +0200

    Transaction class basics

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,2 +1,28 @@
 public class Transaction {
+    private final String iban;
+    private final int account_id;
+    private final float ammount;
+    private int commision;
+
+    public Transaction(String iban_, int account_id_, float ammount_){
+        iban = iban_;
+        account_id = account_id_;
+        ammount = ammount_;
+    }
+
+    String getIban(){
+        return iban;
+    }
+
+    int getAccount_id(){
+        return account_id;
+    }
+
+    float getAmmount(){
+        return ammount;
+    }
+
+    int getCommision(){
+        return commision;
+    }
 }
