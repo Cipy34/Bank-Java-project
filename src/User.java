022e72a8e6b38337d6a071a457722e60530ce0f1
Cipy348 <cipriangabriel112@gmail.com>
2023-12-09 21:10:02 +0200

    User class basics

---
+++ src/User.java
@@ -0,0 +1,22 @@
+public class User {
+    private String user, password;
+    private int userid;
+
+    public User(){
+        user = "";
+        password = "";
+    }
+
+    public User(String user_, String password_, int userid_){
+        user = user_;
+        password = password_;
+        userid = userid_;
+    }
+
+    void setUser(String user_){
+        user = user_;
+    }
+    void setPassword(String password_){
+        password = password_;
+    }
+}
